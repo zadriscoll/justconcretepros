6781fbf903fe0c436336a614979cc8b80a558331
Zack Driscoll <112503139+zadriscoll@users.noreply.github.com>
2025-03-12 00:10:29 -0500

    Create button.js

---
+++ just_concrete_pros/components/ui/button.js
@@ -0,0 +1,7 @@
+export function Button({ children, className, ...props }) {
+  return (
+    <button className={`px-4 py-2 rounded ${className}`} {...props}>
+      {children}
+    </button>
+  );
+}
