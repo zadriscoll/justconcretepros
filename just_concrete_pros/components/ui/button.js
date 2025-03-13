2183e2d608cd64fed776f87b9439962cfcef41e0
Zack Driscoll <112503139+zadriscoll@users.noreply.github.com>
2025-03-13 17:41:21 -0500

    Delete just_concrete_pros directory

--- just_concrete_pros/components/ui/button.js
+++
@@ -1,7 +0,0 @@
-export function Button({ children, className, ...props }) {
-  return (
-    <button className={`px-4 py-2 rounded ${className}`} {...props}>
-      {children}
-    </button>
-  );
-}
